9089e547de939245e90f29774896486d2561317f
Shankalpa-D <DuwadiShankalpa@gmail.com>
2025-11-12 10:36:09 -0500

    created add square function , refactored orgnal loop , attached add square funtion to the button.

--- squares.js
+++ squares.js
@@ -1,7 +1,10 @@
 window.onload = function() {
+    var add = this.document.getElementById("add");
+    add.onclick = addSquare;
     var squarearea = document.getElementById("squarearea");
+    var squareCount = parseInt(Math.random() *21)+30
 
-var squareCount = parseInt(Math.random() *21)+30
+
 
 
 for (let i = 0; i < squareCount; i++) {
@@ -9,7 +12,25 @@ for (let i = 0; i < squareCount; i++) {
     newSquare.className = "square";
     newSquare.style.left = parseInt(Math.random() * 651) + "px";
     newSquare.style.top = parseInt(Math.random() * 251) + "px";
+    newSquare.style.backgroundColor = getRandonColor();
     squarearea.appendChild(newSquare);}
 
 }
         
+function getRandonColor() {
+    var possibilities  = "0123456789ABCDEF";
+    var result = "#";
+    for (var i = 0 ; i < 6; i++) {
+        result += possibilities.charAt(parseInt(Math.random() * possibilities.length));}
+    return result;
+};
+
+function addSquare() {
+    var squarearea = document.getElementById("squarearea");
+    var newSquare = document.createElement("div");
+    newSquare.className = "square";
+    newSquare.style.left = parseInt(Math.random() * 651) + "px";
+    newSquare.style.top = parseInt(Math.random() * 251) + "px";
+    newSquare.style.backgroundColor = getRandonColor();
+    squarearea.appendChild(newSquare);
+}

eca9fd7a3cc7ea43c32514e30433ed8bc065ccf4
Shankalpa-D <DuwadiShankalpa@gmail.com>
2025-11-12 10:18:52 -0500

    random squares added

--- squares.js
+++ squares.js
@@ -7,6 +7,8 @@ var squareCount = parseInt(Math.random() *21)+30
 for (let i = 0; i < squareCount; i++) {
     var newSquare = document.createElement("div");
     newSquare.className = "square";
+    newSquare.style.left = parseInt(Math.random() * 651) + "px";
+    newSquare.style.top = parseInt(Math.random() * 251) + "px";
     squarearea.appendChild(newSquare);}
 
 }
